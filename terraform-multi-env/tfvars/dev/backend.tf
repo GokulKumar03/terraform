8d88f42788fcf8273636068c30924ef4c8346eed
gokul <gokulkumar0307@gmail.com>
2025-06-05 06:38:45 +0530

    terraform

---
+++ terraform-multi-env/tfvars/dev/backend.tf
@@ -0,0 +1,4 @@
+bucket = "gokul-bucket-remote-state-dev"
+  key    = "multienv-dev"
+  region = "us-east-1"
+  dynamodb_table = "gokul-bucket-remote-state-locking-dev" 
---
+++ terraform-multi-env/tfvars/prod/backend.tf
@@ -0,0 +1,4 @@
+bucket = "gokul-bucket-remote-state-prod"
+  key    = "multienv-prod"
+  region = "us-east-1"
+  dynamodb_table = "gokul-bucket-remote-state-locking-prod" 
